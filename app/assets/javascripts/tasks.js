4f988952efeee61a040705b3be48d01b1a8c5694
Andrew Brink <abrink27@gmail.com>
2016-08-24 21:13:41 +0000

    Final

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,12 @@
 $(function() {
-
   // The taskHtml method takes in a JavaScript representation
   // of the task and produces an HTML representation using
   // <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
       " data-id='" + task.id + "'" +
       checkedStatus +
       '><label>' +
@@ -21,13 +22,21 @@ $(function() {
   // the value of the `done` field
   function toggleTask(e) {
     var itemId = $(e.target).data("id");
+
     var doneValue = Boolean($(e.target).is(':checked'));
+
     $.post("/tasks/" + itemId, {
       _method: "PUT",
       task: {
         done: doneValue
       }
-    });
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+
+    } );
   }
 
   $.get("/tasks").success( function( data ) {
